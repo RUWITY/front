d1f7fa42254c9198f8f6dba10d15013257cf41aa
star <chlvkfksquf@gmail.com>
2023-07-29 02:20:47 +0900

    Clean svg

---
+++ assets/icons/index.ts
@@ -0,0 +1,21 @@
+import AnalyzeIcon from "./analyze.svg";
+import CheckIcon from "./check.svg";
+import HistoryIcon from "./history.svg";
+import LinkIcon from "./link.svg";
+import PageIcon from "./page.svg";
+import SettingIcon from "./setting.svg";
+import ShareIcon from "./share.svg";
+import ThemeIcon from "./theme.svg";
+
+export const Icons = {
+  AnalyzeIcon,
+  CheckIcon,
+  HistoryIcon,
+  LinkIcon,
+  PageIcon,
+  SettingIcon,
+  ShareIcon,
+  ThemeIcon,
+};
+
+export default Icons;
