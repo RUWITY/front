79d2032b8b6dfcf9b380059124deed8aae9a52e2
star <chlvkfksquf@gmail.com>
2023-07-29 17:26:53 +0900

    Modify SignIn page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,12 +2,10 @@
 
 import SignIn from "components/SignIn";
 
-export default async function Page() {
+export default async function SignInPage() {
   return (
     <div className="w-full min-h-screen h-full flex">
-      <div className="flex flex-col text-neutral-800 max-w-[390px] w-full bg-[#FAFAFA] mx-auto relative">
-        <SignIn />
-      </div>
+      <SignIn />
     </div>
   );
 }
--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -6,10 +6,14 @@ export default function Page() {
   const { data: session } = useSession();
 
   return (
-    <div className="w-full min-h-screen h-full flex">
-      <button onClick={() => signIn("kakao")}>
-        <img src="/kakao_login_button.png" />
-      </button>
+    <div className=" mx-auto inline-flex justify-center items-center">
+      <div className="flex flex-col">
+        <div className="mb-2 text-sm">링크는 링크지</div>
+        <img src="/logo.png" className="w-[204px] h-[57px] mb-40" />
+        <button onClick={() => signIn("kakao")}>
+          <img src="/kakao_login_button.png" />
+        </button>
+      </div>
     </div>
   );
 }
