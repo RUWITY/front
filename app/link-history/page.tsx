f63cd7e632ecde61241fd286ab78f56da1ed947b
star <chlvkfksquf@gmail.com>
2023-07-30 21:25:27 +0900

    Modify layout

--- app/link-history/page.tsx
+++ app/link-history/page.tsx
@@ -30,13 +30,13 @@ export default function Page() {
   return (
     <div className="w-full min-h-screen h-full flex bg-[#FAFAFA] px-4">
       <div className="mt-[60px] text-[10px] w-full">
-        <div className="w-full">
+        <div className="w-full space-y-2">
           {urlList.map((item: any) => (
             <div
               className="bg-white px-4 py-2 w-full flex flex-col rounded-lg"
               key={item.id}
             >
-              <div className="flex justify-between">
+              <div className="flex justify-between ">
                 <div className="text-[#A9A9A9]">
                   {String(
                     new Date(item.created_at).getFullYear() - 2000
@@ -60,13 +60,12 @@ export default function Page() {
                 </div>
               </div>
               <div className="flex">
-                <div className="w-[86px] h-12 bg-red-400 mr-1">dsa</div>
+                <img className="w-[86px] h-12 mr-1" src="/defaultImg.png" />
                 <div className="flex flex-col justify-between">
                   [Playlist] 🎧들으면 딱 아는 브루노
                   <div className="text-[#A9A9A9]">조회수 {item.view}</div>
                 </div>
               </div>
-
               <div
                 className="mt-1 text-[#737373]"
                 onClick={async () => {
