c7e49dd84cdd9a123c734632a368012339261cf1
star <chlvkfksquf@gmail.com>
2023-08-02 20:46:34 +0900

    빌드 수정

--- app/redirects/signin/page.tsx
+++ app/redirects/signin/page.tsx
@@ -1,9 +1,7 @@
 import Redirect from "components/Redirect";
 
-export default function Page({ searchParams }: any) {
+export default async function Page({ searchParams }: any) {
   const { access_token, refresh_token } = searchParams;
-
-
 
   return <Redirect access_token={access_token} refresh_token={refresh_token} />
 }
