fcf97bc6f66837910ad9eef70a0b7fd86039adc8
star <chlvkfksquf@gmail.com>
2023-08-02 22:14:47 +0900

    1

---
+++ src/pages/redirects/signin/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+
+import useLocalStorage from "src/hooks/useLocalStorage";
+
+export async function getServerSideProps({ query }: any) {
+
+  const { access_token, refresh_token } = query
+
+
+  return { props: { access_token, refresh_token } }
+}
+
+export default function SignInPage({ access_token, refresh_token }: any) {
+  const router = useRouter();
+
+  const [accessToken, setAccessToken] = useLocalStorage<string | null>("access_token", null);
+  const [refreshToken, setRefreshToken] = useLocalStorage<string | null>("refresh_token", null);
+
+  useEffect(() => {
+    if (accessToken) {
+      router.push('/userinfo')
+    }
+
+    setAccessToken(access_token.replace(/'/g, ""))
+    setRefreshToken(refresh_token.replace(/'/g, ""))
+  }, [accessToken, refresh_token]);
+
+  return <div className="w-full min-h-screen h-full flex bg-white"> 2</div >
+}
+
+
