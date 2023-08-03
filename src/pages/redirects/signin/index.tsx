75a51625449981ac722b42b29bdca4efa7a79643
star <chlvkfksquf@gmail.com>
2023-08-03 17:08:56 +0900

    1

--- src/pages/redirects/signin/index.tsx
+++ src/pages/redirects/signin/index.tsx
@@ -1,21 +1,13 @@
-import type { GetServerSidePropsContext } from 'next'
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { query } = context
-
-  const { access_token, refresh_token } = query
-
-  const maxAgeInSeconds = 48 * 60 * 60;
-
-  context.res.setHeader("set-cookie", `access_token=${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjkwOTgzMTQ0LCJleHAiOjE2OTEwMjYzNDR9.vONdUGVNzZXIGK4m725Z-T8uJudyDHWSCQymedvQk6A"}; max-age=${maxAgeInSeconds}; path=/; samesite=lax; httponly;`)
-
-  context.res.writeHead(303, { Location: '/' })
-  context.res.end()
-
-  return { props: { access_token, refresh_token } }
-}
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function SignInPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/')
+  }, [])
+
   return <></>
 }
 
