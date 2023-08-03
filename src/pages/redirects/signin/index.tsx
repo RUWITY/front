c603fc5877ec634e01579822ed3f4660b0d9f893
star <chlvkfksquf@gmail.com>
2023-08-03 17:42:27 +0900

    1

--- src/pages/redirects/signin/index.tsx
+++ src/pages/redirects/signin/index.tsx
@@ -2,12 +2,10 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import type { GetServerSidePropsContext } from 'next'
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  return { props: { context } }
-}
 
 
-export default function SignInPage({ context }: any) {
+
+export default function SignInPage() {
   const router = useRouter()
 
   useEffect(() => {
